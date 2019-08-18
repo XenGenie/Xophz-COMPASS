44f9f1f259075308a1f747f07b6260845303eeb9
Xopher <xpollard@jdmellberg.com>
2019-08-17 23:17:05 -0700

    :rocket: adding jest and coverage reports

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.vue$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/**/*.{js,vue}', '!src/app.js']
+}
---
+++ src/index.spec.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Index from './index.vue'
+
+// Here are some Jasmine 2.0 tests, though you can
+// use any test runner / assertion library combo you prefer
+describe('Index', () => {
+  // Inspect the raw component options
+  it('has a created hook', () => {
+    expect(typeof Index.created).toBe('function')
+  })
+
+  // Evaluate the results of functions in
+  // the raw component options
+  it('sets the correct default data', () => {
+    expect(typeof Index.data).toBe('function')
+    const defaultData = Index.data()
+    expect(defaultData.wpMenuId).toBe('wpwrap')
+  })
+
+  // Inspect the component instance on mount
+  it('correctly sets the message when created', () => {
+    const vm = new Vue(Index).$mount()
+    expect(vm.wpOpenClass).toBe('wp-responsive-open')
+  })
+
+  // // Mount an instance and inspect the render output
+  // it('renders the correct message', () => {
+  //   const Constructor = Vue.extend(Index)
+  //   const vm = new Constructor().$mount()
+  //   expect(vm.$el.textContent).toBe('bye!')
+  // })
+})
